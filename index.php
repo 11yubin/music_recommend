82a56a793fdfe10a8baf66bd027c51e68f1d5819
11yubin <11yubine@gmail.com>
2023-10-20 04:40:57 +0900

    fix: css 미반영

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>음악 추천 시스템</title>
-    <link rel="stylesheet" type="text/css" href="styles.css"> <!-- CSS 파일 링크 -->
+    <link rel="stylesheet" type="text/css" href="styles.css?after"> <!-- CSS 파일 링크 -->
     <link rel="shortcut icon" href="favicon-32x32.png" />
 </head>
 <body>
